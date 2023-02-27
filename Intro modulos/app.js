6f2552e846f3946ee4121e6aa1d633aba5a5e3b8
Josemar Nascimento <jsm_nascimento@hotmail.com>
2023-02-27 20:01:24 -0300

    Introduction Modules

---
+++ Intro modulos/app.js
@@ -0,0 +1,30 @@
+var calculadora = require("./calculadora");
+
+console.log(calculadora.multiplicacao(10,10));
+console.log(calculadora.soma(2,2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var mostrarSite = true;
+const site = "www.aprenda.com"
+
+console.log("Hello Word!");
+console.log("My name is Josemar!");
+console.log("I study Node.js!");
+
+if(mostrarSite){
+    console.log(site)
+}*/
+
